bfb3ae3275d249fc9fbf370d6087d9537e3e13ed
Rama Bena <ramabena.rb@gmail.com>
2025-01-27 00:40:56 +0700

    Remove unused AuthController and Product model files

--- src/controllers/authController.js
+++
@@ -1,9 +0,0 @@
-class AuthController {
-  register(req, res) {
-    console.log(req.url);
-    res.send('get from ' + req.url);
-  }
-}
-
-const authController = new AuthController();
-export default authController;
