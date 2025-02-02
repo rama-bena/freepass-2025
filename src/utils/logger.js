c8665cda13e96195192a81833a794434b105be4c
Rama Bena <ramabena.rb@gmail.com>
2025-02-02 17:26:31 +0700

    Add unit test registerUser POST /user/register

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,5 +1,8 @@
 import colors from 'colors';
 
+const isTestEnv = process.env.NODE_ENV === 'test';
+const isProdEnv = process.env.NODE_ENV === 'production';
+
 colors.setTheme({
   debug: 'cyan',
   info: 'green',
@@ -14,21 +17,25 @@ function getTimestamp() {
 
 const logger = {
   debug: function (...args) {
+    if (isTestEnv || isProdEnv) return;
     const prefix = `[${getTimestamp()}] [DEBUG]`.debug;
     console.log(prefix, ...args);
   },
 
   info: function (...args) {
+    if (isTestEnv) return;
     const prefix = `[${getTimestamp()}] [INFO]`.info;
     console.info(prefix, ...args);
   },
 
   warn: function (...args) {
+    if (isTestEnv) return;
     const prefix = `[${getTimestamp()}] [WARN]`.warn;
     console.warn(prefix, ...args);
   },
 
   error: function (...args) {
+    if (isTestEnv) return;
     const prefix = `[${getTimestamp()}] [ERROR]`.error;
     console.error(prefix, ...args);
   },
